6200dacc10951168dc9420b59ed366c4e398b544
tygedavis <tyge.davis@gmail.com>
2019-12-16 13:51:27 -0700

    Display test passes

--- src/display/Display.test.js
+++ src/display/Display.test.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-test('', () => {
-  
+import Display from './Display';
+
+test('Displays if gate is open/closed', () => {
+  const locked = true;
+  const closed = true;
+  const tree = render(<Display 
+    locked={locked}
+    closed={closed}
+    />)
+  expect(tree.baseElement).toMatchSnapshot();
 })
